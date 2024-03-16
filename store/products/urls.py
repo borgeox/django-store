c3b9067899cf842298d7da04cbcf34b8401363c6
George Borden <borgeox.dev@gmail.com>
2024-03-16 21:48:43 +0200

    Add filtering and pagination

--- store/products/urls.py
+++ store/products/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('/category/<int:category_id>', products, name='category'),
     path('cart/add/<int:product_id>', add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:order_id>', remove_from_cart, name='remove_from_cart'),
 
